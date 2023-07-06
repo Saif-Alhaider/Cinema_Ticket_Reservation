07038914ea8328f3e4799a5d841d3e8a7c4070d9
Saif Alhaider <saifharith90@gmail.com>
2023-07-06 19:54:39 +0300

    add:arabic resource to seat indicator

--- app/src/main/java/com/example/cinematicketreservation/presentation/ui/screen/cinema_hall/SeatIndicatorSection.kt
+++ app/src/main/java/com/example/cinematicketreservation/presentation/ui/screen/cinema_hall/SeatIndicatorSection.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cinematicketreservation.R
 
 @Composable
 fun SeatIndicatorSection(modifier: Modifier=Modifier) {
@@ -19,17 +21,17 @@ fun SeatIndicatorSection(modifier: Modifier=Modifier) {
     ) {
         SeatIndicator(
             circleColor = Color.White,
-            text = "Available",
+            text = stringResource(R.string.available),
             textColor = Color.White.copy(alpha = .87f)
         )
         SeatIndicator(
             circleColor = Color(0xFF3C3C3C),
-            text = "Taken",
+            text = stringResource(R.string.taken),
             textColor = Color(0xFFB5B5B5)
         )
         SeatIndicator(
             circleColor = Color(0xFFFF5524),
-            text = "Selected",
+            text = stringResource(R.string.selected),
             textColor = Color.White.copy(alpha = .87f)
         )
     }
